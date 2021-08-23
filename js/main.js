a3c3b9d2ec93bc08e776cd8e27796053043313b6
VadimBolotin <71722806+VadimBolotin@users.noreply.github.com>
2021-08-23 13:33:10 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$(function(){
+  $('.fairy-tail__slider').slick({
+    prevArrow:'<button type="button" class="slick-btn slick-prev"> <img src="images/svg/arrow-prev.svg" alt=""></button>',
+    nextArrow:'<button type="button" class="slick-btn slick-next"> <img src="images/svg/arrow-next.svg" alt=""></button>',
+    autoplay:true,
+    fade: true,
+    responsive:[/*убираю стрелки приразрешении меньше  */
+      {
+        breakpoint:601,
+        settings:{
+          arrows:false
+        }
+      },
+    ]
+    
+  });
+
+  $('.our-trip__slider').slick({
+    prevArrow:'<button type="button" class="slick-btn slick-prev"> <img src="images/svg/arrow-prev.svg" alt=""></button>',
+    nextArrow:'<button type="button" class="slick-btn slick-next"> <img src="images/svg/arrow-next.svg" alt=""></button>',
+    autoplay:true,
+    fade: true,
+    responsive:[/*убираю стрелки приразрешении меньше  */
+      {
+        breakpoint:601,
+        settings:{
+          arrows:false
+        }
+      },
+    ]
+  });
+});
